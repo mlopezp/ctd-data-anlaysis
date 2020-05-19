bbd9a4cfbb052cf3b240a1d7eba86e0942eae3b6
Mauricio Lopez <mlopezp@users.noreply.github.com>
2020-05-19 23:46:56 +0300

    cleaned up plot code

--- Scripts/Read_Data_From_Files.R
+++ Scripts/Read_Data_From_Files.R
@@ -20,7 +20,7 @@ file.names <- list.files(path = "./", pattern = "*.cnv")
 # read cnv files into a list of lists called ctd_objects
 ctd_objects <- map(file.names, read.ctd)
 
-# trim data to downcast and reduce data to 10 cm intervals. Only works on full data!! breaks when using average
+# trim data to downcast and reduce data to 10 cm intervals. Only works on full data!! breaks when using averaged cnvs
 ctd_trimmed <- ctd_objects %>%
   map(ctdTrim, method = "downcast") %>%
   map(ctdDecimate, p = 0.1)
@@ -71,12 +71,13 @@ site.names <- set_names(site.names) # use unique names for each site for labels 
 # plots function ####
 plots <- function(dataframe, sitename) { # plots function requires a dataframe and site name as parameters
 
-#ggplot object - set defaults for following plots
+# ggplot object - set defaults for following plots
   p <- ggplot(data = dataframe %>%
               na.omit() %>%
               filter(site == sitename),
-              aes(color = Quarter, group = Date)) +
-  geom_path() +
+              aes(y = depth, color = Quarter, group = Date)) + # used date for grouping because in some cases 2 drops were donde in one quarter
+  geom_path() + # to draw profiles as lines 
+  scale_x_continuous(position = "top") +
   scale_y_reverse(breaks = seq(0,40,5)) +
   theme_bw() +
   theme(axis.text = element_text(size = 12, colour = 1),
@@ -87,52 +88,48 @@ plots <- function(dataframe, sitename) { # plots function requires a dataframe a
 
 # temperature plot
 temp <- p +
-  aes(x = temperature, y = depth) +
-  scale_x_continuous(#breaks = seq(15, 36, 0.5)
-    position = "top") +
+  aes(x = temperature) +
+  scale_x_continuous(#breaks = seq(15, 36, 0.5) +
   labs(x = expression(~Temperature~(degree~C)), y = Depth~(m))
 
 # salinity plot
 salinity<- p +
-  aes(x = salinity, y = depth) +
-  scale_x_continuous(#breaks = seq(39,41,.05),
-    position = "top") +
+  aes(x = salinity) +
+  #scale_x_continuous(breaks = seq(39,41,.05) +
   labs(x = expression(~Salinity~(PSU))) +
   theme(axis.title.y.left = element_blank())
 
 # dissolve oxygen plot
 oxygen<- p +
-  aes(x = oxygen, y = depth) +
-  scale_x_continuous(#breaks = seq(6.0,7.0,.1),
-    position = "top") +
+  aes(x = oxygen) +
+  #scale_x_continuous(breaks = seq(6.0,7.0,0.1) +
   labs(x = expression(~DO~(mgL^{-3}))) +
   theme(axis.title.y.left = element_blank())
 
 # fluorescence plot
 fluorescence<- p +
-  aes(x = fluorescence, y = depth) +
-  scale_x_continuous(#breaks = seq(0, 1.1, .2),
-    position = "top") +
+  aes(x = fluorescence) +
+  #scale_x_continuous(breaks = seq(0, 1.1,0.2) +
   labs(x = expression(~Fluorescence~(mgm^{-3})),y = Depth~(m))
 
 # turbidity plot
 turbidity<- p +
-  aes(x = turbidity, y = depth) +
-  scale_x_continuous(#breaks = seq(0.2,1.3,.1),
-    position = "top") +
+  aes(x = turbidity) +
+  #scale_x_continuous(breaks = seq(0.2,1.3,0.1) +
   labs(x = expression(~Turbidity~(NTU))) +
   theme(axis.title.y.left = element_blank())
 
-# Map plot
-
+# Map plot ####
 theme_set(theme_bw())
 
+# create the world map
 world <- ne_countries(scale = "medium", returnclass = "sf",
                       country = c("Qatar", "Bahrain", "Saudi Arabia", "Iran"))
 class(world)
 world_points<- st_centroid(world)
 world_points <- cbind(world, st_coordinates(st_centroid(world$geometry)))
 
+# plot the site location on the map
 map<-ggplot(data = world) +
   geom_sf() +
   coord_sf(xlim = c(49, 54),
@@ -142,31 +139,38 @@ map<-ggplot(data = world) +
   geom_text(data= world_points,
             aes(x=X, y=Y, label=name))+
   geom_point(data = dataframe %>%
-               filter(site == sitename),
-             aes(x = longitude, y = latitude),
-             size = 3,
-             shape = 16)+
-  ggtitle("Sampling Location")+
+                    filter(site == sitename),
+                    aes(x = longitude, y = latitude),
+                    size = 3, shape = 16)+
+  ggtitle("Sampling Location") +
   theme(plot.title= element_text(hjust = 0.5, vjust = -8),
         axis.title.x = element_blank(),
         axis.title.y = element_blank()) +
-  annotate(geom = "text", x = 52.5, y = 26.5, label = "Persian Gulf",
-           fontface = "italic", color = "grey22", size = 4,angle = 325)
+  annotate(geom = "text", 
+           x = 52.5, 
+           y = 26.5, 
+           label = "Persian Gulf",
+           fontface = "italic", 
+           color = "grey22", 
+           size = 4,
+           angle = 325)
 
 #Creating plot grid#####
 # grid with all plots
-P1 <- plot_grid(temp,salinity, map,
-              fluorescence, turbidity,oxygen,
-              nrow = 2, align = "h",label_size = 12)
+P1 <- plot_grid(temp,salinity, map,fluorescence, turbidity,oxygen,
+                nrow = 2, align = "h",label_size = 12)
 # title object
 title <- ggdraw() +
-  draw_label(sitename)
+         draw_label(sitename)
 
 # legend object
-legend <- get_legend(temp + theme(legend.position = "bottom"))
+legend <- get_legend(temp + 
+          theme(legend.position = "bottom"))
 
 # grid with title, plots and legend
-plot_grid(title, P1, legend, ncol=1, rel_heights = c(0.05, 1, 0.05))
+plot_grid(title, P1, legend, 
+          ncol=1, 
+          rel_heights = c(0.05, 1, 0.05))
 }
 
 # Loop to make separte pages for each site ####
@@ -174,19 +178,19 @@ site_plots <- map(site.names, ~plots(df, .x))
 
 # to run this for one site replace site.names with the site name in quotes ####
 # eg.
-map("Umm Al Arshan", ~plots(df, .x))
+#map("Umm Al Arshan", ~plots(df, .x))
 
 # save files ####
 
 # save each grid as a page in  pdf in the Figures folder
-pdf("./../Figures/all sites.pdf", height = 5, width = 7)
-site_plots
-dev.off()
+#pdf("./../Figures/all sites.pdf", height = 5, width = 7)
+#site_plots
+#dev.off()
 
 # save each grid as individual png
-plot.names <- paste0(site.names, ".png") #generate names for each plot
-walk2(plot.names, site_plots, ~ggsave(filename = .x, plot = .y,
-                                      path = "../Figures/",
-                                      height = 5, width = 7))
+#plot.names <- paste0(site.names, ".png") #generate names for each plot
+#walk2(plot.names, site_plots, ~ggsave(filename = .x, plot = .y,
+#                                       path = "../Figures/",
+#                                       height = 5, width = 7))
 
 
